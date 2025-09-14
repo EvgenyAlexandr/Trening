50bb8710798e55bb51db5ff79cc45765f6f2a7c9
EvgenyAlexand <bragin-e@mail.ru>
2025-09-14 19:17:17 +0700

    List - В процессе понимания.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,51 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Java Collections Framework: List
+        /* Основные интерфейсы:
+            • List - упорядоченные элементы с дубликатами
+            • Set - уникальные элементы без порядка
+            • Map - пары ключ-значение
+         */
+
+        // ArrayList
+        System.out.println("List - ArrayList");
+
+        List<String> list_String = new ArrayList<>();
+        list_String.add("Один");
+        list_String.add("Два");
+        list_String.add("Три");
+        list_String.add("Четыре");
+        list_String.add("Пять");
+
+        System.out.println("list_String = " + list_String);
+        // В консоле отобразится -> list_String = [Один, Два, Три, Четыре, Пять]
+
+        // Получение итератора
+        Iterator<String> iterator = list_String.iterator();
+
+        // Перебор элементов с использованием итератора
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
 
 
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//
+//        // Получение итератора
+//        Iterator<Integer> iterator = list.iterator();
+//
+//        // Перебор элементов с использованием итератора
+//        while (iterator.hasNext()) {
+//            Integer element = iterator.next();
+//            System.out.println(element);
+//        }
 
 
     }
